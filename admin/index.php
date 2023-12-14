aa7c00442cef04bd6958b6782425539a750a12cf
rafirh <rafirahmann18@gmail.com>
2023-12-14 21:48:58 +0700

    update: index admin

--- admin/index.php
+++ admin/index.php
@@ -1,10 +1,138 @@
 <?php
 $active = 'beranda';
 $title = 'Beranda';
+
+include('partials/header.php');
+
+$jumlah_pelanggan = countAll($conn, 'user', 'role', 'customer');
+$jumlah_agen = countAll($conn, 'user', 'role', 'agent');
+$jumlah_mobil = countAll($conn, 'mobil');
+$jumlah_transaksi = countAll($conn, 'transaksi');
 ?>
 
-<?php include('partials/header.php') ?>
+<div class="page-header d-print-none mt-2">
+  <div class="container-xl">
+    <div class="row g-2 align-items-center">
+      <div class="col">
+        <h3 class="page-title">
+          Beranda
+        </h3>
+      </div>
+    </div>
+  </div>
+</div>
 
-  
+<div class="page-body">
+  <div class="container-xl">
+    <div class="row">
+      <div class="col-sm-6 col-xl-4 mb-3">
+        <div class="card card-sm">
+          <div class="card-body">
+            <div class="row align-items-center">
+              <div class="col-auto">
+                <span class="bg-teal text-white avatar">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-users" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M9 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" />
+                    <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+                    <path d="M16 3.13a4 4 0 0 1 0 7.75" />
+                    <path d="M21 21v-2a4 4 0 0 0 -3 -3.85" />
+                  </svg>
+                </span>
+              </div>
+              <div class="col">
+                <div class="fw-bold">
+                  <?= $jumlah_pelanggan ?>
+                </div>
+                <div class="text-muted">
+                  total pelanggan
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6 col-xl-4 mb-3">
+        <div class="card card-sm">
+          <div class="card-body">
+            <div class="row align-items-center">
+              <div class="col-auto">
+                <span class="bg-indigo text-white avatar">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-building-store" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M3 21l18 0" />
+                    <path d="M3 7v1a3 3 0 0 0 6 0v-1m0 1a3 3 0 0 0 6 0v-1m0 1a3 3 0 0 0 6 0v-1h-18l2 -4h14l2 4" />
+                    <path d="M5 21l0 -10.15" />
+                    <path d="M19 21l0 -10.15" />
+                    <path d="M9 21v-4a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v4" />
+                  </svg>
+                </span>
+              </div>
+              <div class="col">
+                <div class="fw-bold">
+                  <?= $jumlah_agen ?>
+                </div>
+                <div class="text-muted">
+                  total agen
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6 col-xl-4 mb-3">
+        <div class="card card-sm">
+          <div class="card-body">
+            <div class="row align-items-center">
+              <div class="col-auto">
+                <span class="bg-purple text-white avatar">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-car" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M7 17m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+                    <path d="M17 17m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+                    <path d="M5 17h-2v-6l2 -5h9l4 5h1a2 2 0 0 1 2 2v4h-2m-4 0h-6m-6 -6h15m-6 0v-5" />
+                  </svg>
+                </span>
+              </div>
+              <div class="col">
+                <div class="fw-bold">
+                  <?= $jumlah_mobil ?>
+                </div>
+                <div class="text-muted">
+                  total mobil
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6 col-xl-4 mb-3">
+        <div class="card card-sm">
+          <div class="card-body">
+            <div class="row align-items-center">
+              <div class="col-auto">
+                <span class="bg-purple text-white avatar">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrows-sort" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M3 9l4 -4l4 4m-4 -4v14" />
+                    <path d="M21 15l-4 4l-4 -4m4 4v-14" />
+                  </svg>
+                </span>
+              </div>
+              <div class="col">
+                <div class="fw-bold">
+                  <?= $jumlah_transaksi ?>
+                </div>
+                <div class="text-muted">
+                  total pesanan
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
 <?php include('partials/footer.php') ?>
