4e608e4f1cdb71f7bd1f96b3659817a8fa5c7bbb
rafirh <rafirahmann18@gmail.com>
2023-12-18 23:44:18 +0700

    remove btn verifikasi

--- agent/verifikasi-pelunasan.php
+++ agent/verifikasi-pelunasan.php
@@ -154,7 +154,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     </td>
                     <td>
                       <div class="d-flex justify-content-center">
-                        <button class="btn btn-icon btn-pill bg-success-lt me-1" data-id="<?= $item['id'] ?>" data-bs-toggle="modal" data-bs-target="#modalAccept">
+                        <!-- <button class="btn btn-icon btn-pill bg-success-lt me-1" data-id="<?= $item['id'] ?>" data-bs-toggle="modal" data-bs-target="#modalAccept">
                           <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-check" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                             <path d="M5 12l5 5l10 -10" />
@@ -166,7 +166,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                             <path d="M18 6l-12 12" />
                             <path d="M6 6l12 12" />
                           </svg>
-                        </button>
+                        </button> -->
                         <button class="btn btn-icon btn-pill bg-muted-lt" data-bs-toggle="dropdown" aria-expanded="false" title="Lainnya">
                           <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-dots-vertical" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
